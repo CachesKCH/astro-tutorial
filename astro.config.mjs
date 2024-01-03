7f3dacfb6779db5f6bc7a719bf11a882e7b2b8e9
Gustavo Dominguez <gfdominguez119@gmail.com>
2024-01-03 10:29:10 -0300

    Finish static components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://astro-tutorial-caches.netlify.app/',
+  integrations: [tailwind(), preact()]
 });
